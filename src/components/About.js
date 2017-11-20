e3871014a97243b0465e49e5408670f161db2ae8
Heiko Wenninger <heiko.wenninger@dm.de>
2017-11-20 20:49:03 +0100

    about and home added

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import about from './about.jpg';
 
-export default function About () {
-  return (
-    <h2 className="text-center">
-     About us: ...
-    </h2>
-  )
+export default function About() {
+	return (
+		<div>
+			<h2 className="text-center">
+				About us
+			</h2>
+			<br/>
+			<div className="row">
+				<div className="col-md-4">
+					<img src={about} alt="about us" className="img-thumbnail"/>
+				</div>
+				<div className="col-md-4">
+					<p>
+						Erdal Demirtas<br/>
+						<a href="mailto:erdal.demirtas@cgi.com">erdal.demirtas@cgi.com</a><br/><br/>
+						Heiko Wenninger<br/>
+						<a href="mailto:heiko.wenninger@cgi.com">heiko.wenninger@cgi.com</a>
+					</p>
+					<a href="https://www.de.cgi.com/" target="_blank" rel="noopener noreferrer">
+						<img src="https://www.de.cgi.com/sites/all/themes/cgi/logo.png" alt="cgi logo"/>
+					</a>
+				</div>
+			</div>
+		</div>
+	)
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import reactLogo from './reactLogo.png';
 
 export default function Home() {
 	return (
@@ -6,17 +7,29 @@ export default function Home() {
 			<h2 className="text-center">
 				React HandsOn Session
 			</h2>
-			<p>
-				Wikipedia: <i>
-				React ist eine JavaScript-Softwarebibliothek[3], die ein Grundgerüst für die Ausgabe von
-				User-Interface-Komponenten von Webseiten zur Verfügung stellt (Webframework). Komponenten werden in
-				React hierarchisch aufgebaut und in dessen Syntax als selbst definierte HTML-Tags repräsentiert.[4] Das
-				Modell von React verspricht durch die Konzepte des unidirektionalen Datenflusses und des "Virtual Dom"
-				den einfachen, aber trotzdem performanten Aufbau auch komplexer Anwendungen. React bildet typischerweise
-				die Basis für Single-Page-Webanwendungen, kann jedoch auch mittels Node.js serverseitig (vor-)gerendert
-				werden.
-			</i>
-			</p>
+			<br/>
+			<div className="row">
+				<div className="col-md-4">
+					<img src={reactLogo} alt="React Logo" className="img-thumbnail"/>
+				</div>
+				<div className="col-md-4">
+					<p>
+						Wikipedia:
+						React ist eine JavaScript-Softwarebibliothek[3], die ein Grundgerüst für die Ausgabe von
+						User-Interface-Komponenten von Webseiten zur Verfügung stellt (Webframework). Komponenten werden
+						in
+						React hierarchisch aufgebaut und in dessen Syntax als selbst definierte HTML-Tags
+						repräsentiert.[4] Das
+						Modell von React verspricht durch die Konzepte des unidirektionalen Datenflusses und des
+						"Virtual Dom"
+						den einfachen, aber trotzdem performanten Aufbau auch komplexer Anwendungen. React bildet
+						typischerweise
+						die Basis für Single-Page-Webanwendungen, kann jedoch auch mittels Node.js serverseitig
+						(vor-)gerendert
+						werden.
+					</p>
+				</div>
+			</div>
 		</div>
 	)
 }
